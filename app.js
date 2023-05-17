dd09d91cc1d61b8f60e979ea0eebc1a5f4c2af6c
pranav2805 <pranavpradeep095@gmail.com>
2023-05-17 16:13:55 +0530

    Writing & reading from file

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
+
     if(url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>')
-        res.write('</html>');
+        fs.readFile('message.txt', (err, data) => {
+            res.write('<html>');
+            res.write('<head><title>Enter Message</title></head>');
+            res.write(`<body><p>${data}</p>`)
+            res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>')
+            res.write('</html>');
+            return res.end();
+        })
+        // res.write('<html>');
+        // res.write('<head><title>Enter Message</title></head>');
+        // res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>')
+        // res.write('</html>');
+        // return res.end();
+    }
+
+    if(url === '/message' && method==='POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            console.log(message);
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
@@ -27,8 +56,8 @@ const server = http.createServer((req, res) => {
     }
 
     if(url === '/node') {
-        // console.log(req.url, req.method, req.headers);
-        // res.setHeader('Content-Type', 'text/html');
+        //console.log(req.url, req.method, req.headers);
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
         res.write('<body><h1>Welcome to my Node Js project</h1></body>')
