da55380b1b1dc64fff25839dce7dc5b6647c3ca9
pranav2805 <pranavpradeep095@gmail.com>
2023-05-19 19:08:02 +0530

    Middlewares Intro

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-//const http = require('http');
-
-//const routes = require('./routes');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware!");
-    next();  //Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+// app.use('/', (req, res, next) => {
+//     console.log("This always run!");
+//     next();  //Allows the request to continue to the next middleware in line
+// });
+
+app.use('/add-product', (req, res, next) => {
+    //console.log("In another middleware!");
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="title"><input type="text" name="size" placeholder="size"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
-app.use((req, res, next) => {
-    console.log("In another middleware!");
-    //res.send('<h1>Hello from Express!</h1>');
-    res.send( { key1: value })
-})
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log("In another middlewaressss!");
+    res.send('<h1>Hello from Express!</h1>');
+});
 
 app.listen(3000);
